b63b6a9b9d2629f71fa90c7915d783a29cdccb76
UsamaArshad16 <usama.arshad@xavor.com>
2024-07-12 19:11:18 +0500

    Absolute paths are added and Service is created

--- data_uploading.sh
+++ data_uploading.sh
@@ -7,9 +7,13 @@
 echo "Starting data uploading Script--------------------------------"
 
 # Start the Python scripts with the dynamic username
-python3 /home/orin2/uci_data_recording/rgb_uploading.py & rgb_pid=$!
-python3 /home/orin2/uci_data_recording/point_cloud_uploading.py & pc_pid=$!
-python3 /home/orin2/uci_data_recording/audio_uploading.py & audio_pid=$!
+# python3 /home/orin2/uci_data_recording/rgb_uploading.py & rgb_pid=$!
+# python3 /home/orin2/uci_data_recording/point_cloud_uploading.py & pc_pid=$!
+# python3 /home/orin2/uci_data_recording/audio_uploading.py & audio_pid=$!
+
+python3 /home/$USERNAME/uci_data_recording/rgb_uploading.py & rgb_pid=$!
+python3 /home/$USERNAME/uci_data_recording/point_cloud_uploading.py & pc_pid=$!
+python3 /home/$USERNAME/uci_data_recording/audio_uploading.py & audio_pid=$!
 
 # Trap Ctrl+C and forcefully terminate processes
 trap 'kill -9 $rgb_pid $pc_pid $audio_pid' INT
