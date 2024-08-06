fed69938c459e07023561b0778cf73725b479919
Usama Arshad <115062082+UsamaArshad16@users.noreply.github.com>
2024-08-06 23:41:52 +0500

    Update setup_recordings_xavier.sh

--- setup_recordings_xavier.sh
+++ setup_recordings_xavier.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 export DISPLAY=:0
 sleep 3
+
+# Get the current username
+USERNAME=$(whoami)
+echo "$USERNAME"
+
 # Run the record_azure command in the background and store the process ID
-/home/orin2/uci_data_recording/record_azure & record_azure_pid=$!
-python3 /home/orin2/uci_data_recording/record_audio.py & record_audio_pid=$!
+/home/$USERNAME/uci_data_recording/record_azure & record_azure_pid=$!
+python3 /home/$USERNAME/uci_data_recording/record_audio.py & record_audio_pid=$!
 # Wait for 15 seconds
 sleep 5
 
 # Run the python3 rgb_uploading.py command in the background and store the process ID
-python3 /home/orin2/uci_data_recording/rgb_uploading.py & rgb_pid=$!
-python3 /home/orin2/uci_data_recording/point_cloud_uploading.py & pc_pid=$!
-python3 /home/orin2/uci_data_recording/audio_uploading.py & audio_pid=$!
+python3 /home/$USERNAME/uci_data_recording/rgb_uploading.py & rgb_pid=$!
+python3 /home/$USERNAME/uci_data_recording/point_cloud_uploading.py & pc_pid=$!
+python3 /home/$USERNAME/uci_data_recording/audio_uploading.py & audio_pid=$!
 
 # Trap Ctrl+C and forcefully terminate processes
 trap 'kill -9 $record_azure_pid $record_audio_pid $rgb_pid $pc_pid $audio_pid' INT
